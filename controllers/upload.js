91f85cddf1324bedab1fc4e7814f806e5028cd61
ledys09 <alvaradoestefany51@yahoo.es>
2020-10-27 23:59:03 -0600

    CRUD archivos de empresa

--- controllers/upload.js
+++ controllers/upload.js
@@ -2,6 +2,8 @@ const Usuario = require('../models/user');
 const Upload = require('../models/upload')
 const fs = require('fs');
 const pathD = require('path');
+const _ = require('underscore');
+
 
 
 
@@ -9,136 +11,264 @@ const pathD = require('path');
 //@route    PUT /api/upload/img-perfil
 //@access   Private (auth)
 exports.imgPerfil = async(req, res) => {
-    let tipoUser = '';
-    const usuario = req.usuario;
-    if (usuario.role == 'client_role') {
-        tipoUser = 'client';
-    }
-    if (usuario.role == 'enterprise_role') {
-        tipoUser = 'enterprise';
-    }
-    if (usuario.role == 'admin_role') {
-        tipoUser = 'admin';
-    }
-    if (!req.files) {
-        return res.status(400).json({
-            success: false,
-            msg: 'No seleccionó imagen',
-            error: { message: 'Debe seleccionar una imagen' }
-        })
-    }
-
-    const archivoSubir = req.files.archivoSubir
-    const nombreDividido = archivoSubir.name.split('.');
-    const extension = nombreDividido[nombreDividido.length - 1]
-    const extensionesValidas = ['png', 'jpg', 'gif', 'jpeg']
-
-    if (!extensionesValidas.includes(extension)) {
-        return res.status(400).json({
-            success: false,
-            error: { msg: 'Extensión no válida' }
-        })
-    }
-
-    const nombreArchivo = `${ usuario._id }-${new Date().getMonth()}.${extension}`;
-    const path = `./uploads/imgProfile/${tipoUser}/${nombreArchivo}`;
-    archivoSubir.mv(path, (err) => {
-        if (err) {
+    try {
+        let tipoUser = '';
+        const usuario = req.usuario;
+        if (usuario.role == 'client_role') {
+            tipoUser = 'client';
+        }
+        if (usuario.role == 'enterprise_role') {
+            tipoUser = 'enterprise';
+        }
+        if (usuario.role == 'admin_role') {
+            tipoUser = 'admin';
+        }
+        if (!req.files) {
             return res.status(400).json({
                 success: false,
-                msg: 'No se movió la imagen',
-                err
+                msg: 'No seleccionó imagen',
+                error: { message: 'Debe seleccionar una imagen' }
             })
         }
-    });
-    // asignar foto a un usuario
-    await Usuario.findById(usuario._id, (err, usuario) => {
-        if (err) {
+
+        const archivoSubir = req.files.archivoSubir
+        const nombreDividido = archivoSubir.name.split('.');
+        const extension = nombreDividido[nombreDividido.length - 1]
+        const extensionesValidas = ['png', 'jpg', 'gif', 'jpeg']
+
+        if (!extensionesValidas.includes(extension)) {
             return res.status(400).json({
                 success: false,
-                msg: 'No se encontro usuario',
-                err
+                error: { msg: 'Extensión no válida' }
             })
         }
-        const pathBefore = `./uploads/imgProfile/${tipoUser}/${usuario.foto}`
-        if (fs.existsSync(pathBefore)) {
-            fs.unlinkSync(pathBefore);
-        }
-        usuario.foto = nombreArchivo;
-        usuario.save((err, usuarioActualizado) => {
+
+        const nombreArchivo = `${ usuario._id }-${new Date().getMonth()}.${extension}`;
+        const path = `./uploads/imgProfile/${tipoUser}/${nombreArchivo}`;
+        archivoSubir.mv(path, (err) => {
             if (err) {
                 return res.status(400).json({
                     success: false,
-                    msg: 'No se actulizo foto del usuario',
+                    msg: 'No se movió la imagen',
                     err
                 })
             }
-            return res.status(200).json({
-                success: true,
-                msg: 'Se actualizó foto del usuario',
-                data: usuarioActualizado
+        });
+        // asignar foto a un usuario
+        await Usuario.findById(usuario._id, (err, usuario) => {
+            if (err) {
+                return res.status(400).json({
+                    success: false,
+                    msg: 'No se encontró usuario',
+                    err
+                })
+            }
+            const pathBefore = `./uploads/imgProfile/${tipoUser}/${usuario.foto}`
+            if (fs.existsSync(pathBefore)) {
+                fs.unlinkSync(pathBefore);
+            }
+            usuario.foto = nombreArchivo;
+            usuario.save((err, usuarioActualizado) => {
+                if (err) {
+                    return res.status(400).json({
+                        success: false,
+                        msg: 'No se actulizó foto del usuario',
+                        err
+                    })
+                }
+                return res.status(200).json({
+                    success: true,
+                    msg: 'Se actualizó foto del usuario',
+                    data: usuarioActualizado
+                });
             });
         });
-    });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            msg: 'Error interno del servidor',
+            error
+        });
+    }
 };
 
 //@desc     Subir archivos empresa
 //@route    POST /api/upload/files
 //@access   Private (enterprise_role)
 exports.filesUser = async(req, res) => {
-    const usuario = req.usuario;
-    const idEmpresa = usuario._id;
+    try {
+        const usuario = req.usuario;
+        const idEmpresa = usuario._id;
 
-    if (!req.files) {
-        return res.status(400).json({
+        if (!req.files) {
+            return res.status(400).json({
+                success: false,
+                msg: 'No seleccionó  archivo',
+                error: { message: 'Debe seleccionar un archivo' }
+            })
+        }
+        //console.log(req.files)
+        const archivoSubir = req.files.archivoSubir
+        const tipoDividido = archivoSubir.mimetype.split('/');
+        const tipoArchivo = tipoDividido[0]
+        const nombreDividido = archivoSubir.name.split('.');
+        const extension = nombreDividido[nombreDividido.length - 1]
+        const nombreArchivo = `${ usuario._id }-${ new Date().getMilliseconds() }.${ extension }`;
+        fs.mkdir(pathD.join(__dirname, `../uploads/filesEnterprise/${idEmpresa}`), { recursive: true }, (err) => {
+            if (err) {
+                return res.status(400).json({
+                    success: false,
+                    msg: 'No se creó el directorio',
+                    err
+                })
+            }
+        });
+        const path = `./uploads/filesEnterprise/${idEmpresa}/${nombreArchivo}`;
+        archivoSubir.mv(path, (err) => {
+            if (err) {
+                return res.status(400).json({
+                    success: false,
+                    msg: 'No se movió la imagen',
+                    err
+                })
+            }
+        });
+        let tipoDB = '';
+        if (tipoArchivo != 'image' && tipoArchivo != 'video') {
+            tipoDB = 'others';
+        } else {
+            tipoDB = tipoArchivo;
+        }
+        let newFile = new Upload({
+            nombreArchivo,
+            tipo: tipoDB,
+            url: path,
+            usuario: usuario._id
+        })
+        await newFile.save(err => {
+            if (err) {
+                return res.status(400).json({
+                    success: false,
+                    msg: 'Error al guardar archivo',
+                    err
+                })
+            }
+            return res.status(201).json({
+                success: true,
+                msg: 'Archivo guardado',
+                data: newFile
+            });
+        })
+    } catch (error) {
+        return res.status(500).json({
             success: false,
-            msg: 'No seleccionó  archivo',
-            error: { message: 'Debe seleccionar un archivo' }
+            msg: 'Error interno del servidor',
+            error
         })
     }
-
-    const archivoSubir = req.files.archivoSubir
-    const nombreDividido = archivoSubir.name.split('.');
-    const extension = nombreDividido[nombreDividido.length - 1]
-    const nombreArchivo = `${ usuario._id }-${ new Date().getMilliseconds() }.${ extension }`;
-    fs.mkdir(pathD.join(__dirname, `../uploads/filesEnterprise/${idEmpresa}`), { recursive: true }, (err) => {
-        if (err) {
-            return res.status(400).json({
-                success: false,
-                msg: 'No se creó el directorio',
-                err
-            })
-        }
-    });
-    const path = `./uploads/filesEnterprise/${idEmpresa}/${nombreArchivo}`;
-    archivoSubir.mv(path, (err) => {
-        if (err) {
-            return res.status(400).json({
-                success: false,
-                msg: 'No se movió la imagen',
-                err
-            })
-        }
-    });
-
-    let newFile = new Upload({
-        nombreArchivo,
-        tipo: extension,
-        url: path,
-        usuario: usuario._id
-    })
-    await newFile.save(err => {
-        if (err) {
-            return res.status(400).json({
-                success: false,
-                msg: 'Error al guardar archivo',
-                err
-            })
-        }
-        return res.status(201).json({
-            success: true,
-            msg: 'Archivo guardado',
-            data: newFile
-        });
-    })
 };
+
+//@desc     Obtener los archivos por tipo
+//@route    GET /api/upload/:tipo
+//@access   Private(enteprise_role)
+exports.files = async(req, res) => {
+    try {
+        const tipo = req.params.tipo
+        const usuario = req.usuario;
+
+        await Upload.find({ tipo: tipo, usuario: usuario._id }, (err, fileDB) => {
+            if (err) {
+                return res.status(400).json({
+                    success: false,
+                    msg: 'Error en la base de datos',
+                    err
+                })
+            }
+            if (fileDB == '') {
+                return res.status(404).json({
+                    success: false,
+                    msg: 'No hay archivos de este tipo',
+                    err
+                })
+            }
+            return res.status(200).json({
+                success: true,
+                msg: 'Archivos',
+                fileDB
+            })
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            msg: 'Error interno del servidor',
+            error
+        })
+    }
+};
+
+//@desc     Actualizar un archivo
+//@route    PUT /api/upload/:id
+//@access   Private(enterprise_role)
+exports.updateFile = async(req, res) => {
+    try {
+        const id = req.params.id;
+        const body = _.pick(req.body, ['nombreArchivo']);
+        Upload.findByIdAndUpdate(id, body, { new: true, runValidators: true, context: 'query' }, (err, fileDB) => {
+            if (err) {
+                return res.status(400).json({
+                    success: false,
+                    msg: 'Error al actualizar',
+                    errors: err
+                })
+            }
+            return res.status(200).json({
+                success: true,
+                msg: 'Archivo Actualizado',
+                fileDB
+            })
+        })
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            success: false,
+            msg: 'Error en el servidor',
+            error
+        })
+    }
+};
+
+//@desc     Eliminar un archivo
+//@route    DELETE /api/upload/:id
+//@access   Private(enterprise_role)
+exports.deleteFile = async(req, res) => {
+    try {
+        const id = req.params.id;
+
+        Upload.findByIdAndRemove(id, (err, fileDB) => {
+            if (err) {
+                return res.status(400).json({
+                    success: false,
+                    msg: 'Error al eliminar',
+                    errors: err
+                })
+            }
+            const pathBefore = fileDB.url;
+            if (fs.existsSync(pathBefore)) {
+                fs.unlinkSync(pathBefore);
+            }
+            return res.status(200).json({
+                success: true,
+                msg: 'Archivo eliminado',
+                fileDB
+            })
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            msg: 'Error en el servidor',
+            error
+        })
+    }
+}
--- routes/upload.js
+++ routes/upload.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const { filesUser, imgPerfil } = require('../controllers/upload');
 const fileUpload = require('express-fileupload');
 const auth = require('../middlewares/auth');
 const authorize = require('../middlewares/authorize')
 const app = express()
+const {
+    filesUser,
+    imgPerfil,
+    files,
+    updateFile,
+    deleteFile
+} = require('../controllers/upload');
+
 
 app.use(fileUpload({ useTempFiles: true }));
 
@@ -11,6 +18,12 @@ app.use(fileUpload({ useTempFiles: true }));
 app.route("/img-perfil").put(auth, imgPerfil);
 
 app.route("/files").post(auth, authorize("enterprise_role"), filesUser);
+
+app.route("/:tipo").get(auth, authorize("enterprise_role"), files);
+
+app.route("/:id").put(auth, authorize("enterprise_role"), updateFile);
+
+app.route("/:id").delete(auth, authorize("enterprise_role"), deleteFile)
 
 
 
